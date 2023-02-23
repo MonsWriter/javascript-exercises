05efd9b1845d36ef068aafd55f2f68a9c4bac4b1
aleksandrkirillov <monswriter@gmail.com>
2023-02-23 15:38:16 +0600

    Pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheitDegrees) {
+  let celsiusDegrees = (farenheitDegrees - 32) * 5 / 9;
+  return round(celsiusDegrees);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegrees) {
+  let farenheitDegrees = (celsiusDegrees * 9 / 5) + 32;
+  return round(farenheitDegrees);
 };
+
+function round(number) {
+  return Math.round(number * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
