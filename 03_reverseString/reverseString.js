6043af5e4b156606b99823e510e342d5fabd44f9
aleksandrkirillov <monswriter@gmail.com>
2023-02-23 13:03:44 +0600

    Pass 'reverses single word' case

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let arrayOfChars = Array.from(inputString);
+    let reversedArrayOfChars = arrayOfChars.reverse();
+    return reversedArrayOfChars.join('');
 };
 
 // Do not edit below this line
