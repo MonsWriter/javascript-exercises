b752f33a9fbb8f4b72f225ea1b9c458ccc82611b
aleksandrkirillov <monswriter@gmail.com>
2023-02-23 14:25:15 +0600

    Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function(inputArray, elementToRemove) {
-    const indexOfElementToRemove = inputArray.indexOf(elementToRemove);
-    inputArray.splice(indexOfElementToRemove, 1);
+const removeFromArray = function() {
+    let args = Array.from(arguments);
+    let inputArray = args.shift();
+    const elementsToRemove = args;
+    elementsToRemove.forEach((element) => {
+        const indexOfElementToRemove = inputArray.indexOf(element);
+        if (indexOfElementToRemove > -1) {
+            inputArray.splice(indexOfElementToRemove, 1);
+        }
+    });
     return inputArray;
 };
 
