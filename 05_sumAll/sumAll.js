8de99428abda4e1023d04839defc5bddef3fa654
aleksandrkirillov <monswriter@gmail.com>
2023-02-23 15:09:24 +0600

    Pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let result = 0; 
+    if (typeof(firstNumber) != 'number' || 
+        typeof(secondNumber) != 'number' || 
+        (firstNumber < 0) || 
+        (secondNumber < 0) ) 
+    {
+        result = 'ERROR';
+    } else {
+        let smallerNumber = (firstNumber > secondNumber) ? secondNumber : firstNumber
+        let largerNumber = (firstNumber > secondNumber) ? firstNumber : secondNumber
+        for (let number = smallerNumber; number < largerNumber + 1; number++) {
+            result += number;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
