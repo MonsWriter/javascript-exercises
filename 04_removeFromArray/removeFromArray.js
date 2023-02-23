88e073b71ed2b842d00acbb1b384eb89b4f2f208
aleksandrkirillov <monswriter@gmail.com>
2023-02-23 13:45:09 +0600

    Implement solution for the first test case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, elementToRemove) {
+    const indexOfElementToRemove = inputArray.indexOf(elementToRemove);
+    inputArray.splice(indexOfElementToRemove, 1);
+    return inputArray;
 };
 
 // Do not edit below this line
