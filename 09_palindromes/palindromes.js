03e2b6a3d44d4c656be26c6546fd333d39fdd897
aleksandrkirillov <monswriter@gmail.com>
2023-03-08 15:53:44 +0600

    Pass all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanedString = clean(string).toUpperCase();
+    const reversedString = reverse(cleanedString);
+    return (cleanedString === reversedString);
 };
+
+function reverse(string) {
+    return string.split("").reverse().join("");
+}
+
+function clean(string) {
+    return string.replace(/\W/g, "");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
